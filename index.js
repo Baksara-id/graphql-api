8d6ef338399a69b3773979104bbf11a51f826a28
Forward489 <glennstevencg21@gmail.com>
2023-05-30 07:36:55 +0000

    Konfigurasi di Cloud done, tapi masih gagal

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const resolvers = require('./resolvers');
 const context = require('./context');
 
 const server = new ApolloServer({ typeDefs: types, resolvers, context });
-const port = 4000;
+// const port = 4000;
 
-server.listen(port).then(({ url }) => {
+server.listen().then(({ url }) => {
   console.log('Server is running at ' + url);
 });
